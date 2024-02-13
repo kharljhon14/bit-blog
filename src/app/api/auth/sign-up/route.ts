9b6658f5e4e54dfb4db08f3f740e1222181db71e
kharle14 <enriquezkharl14@gmail.com>
2024-02-13 16:36:45 +0800

    [Feat] Create user error handling

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,3 +1,4 @@
+import { sql } from '@/db';
 import { schemaValidator } from '@/helpers/schemaValidator';
 import type { CreateUserSchemaType } from '@/schemas/user';
 import { CreateUserSchema } from '@/schemas/user';
@@ -10,7 +11,22 @@ export async function POST(req: Request) {
 
     if (error) return Response.json({ message: error }, { status: 400 });
 
-    return Response.json({ data: 'ok' });
+    const userRes = await sql('select * from users where username = $1', [
+      body.username,
+    ]);
+
+    if (userRes.rowCount !== 0)
+      return Response.json(
+        { message: 'Username already taken!' },
+        { status: 409 }
+      );
+
+    await sql('insert into users (username, password) values ($1, $2)', [
+      body.username,
+      body.password,
+    ]);
+
+    return Response.json({ message: 'Success' });
   } catch (err) {
     if (err instanceof Error)
       return Response.json({ message: err.message }, { status: 500 });
